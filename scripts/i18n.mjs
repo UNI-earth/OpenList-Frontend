4fd4df9354eb514cb8ae5270aae741d6de2d9f75
9527earth <176719848+9527earth@users.noreply.github.com>
2025-08-23 11:31:36 +0800

    Update i18n.mjs

--- scripts/i18n.mjs
+++ scripts/i18n.mjs
@@ -4,15 +4,8 @@ import path from "path"
 const root = "./src/lang"
 const entry = "entry.ts"
 const langs = fs.readdirSync(root)
-
-// 默认语言目录改为 zh-CN
-const defaultLang = "zh-CN"
-
 langs
-  .filter((lang) => lang !== defaultLang)
+  .filter((lang) => lang !== "en")
   .forEach((lang) => {
-    fs.copyFileSync(
-      path.join(root, defaultLang, entry),
-      path.join(root, lang, entry)
-    )
+    fs.copyFileSync(path.join(root, "en", entry), path.join(root, lang, entry))
   })
